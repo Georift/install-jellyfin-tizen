0ceba3c207df27306563a8ded615bcb5e66eb017
Domenico Blanco <doddo97@me.com>
2024-05-16 13:10:23 +0200

    fix: obtain device name from sdb

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ sdb connect $1
 
 # work out the TV name by connecting to $TV_IP:8001/api/v2/ and getting .device.modelName with jq
 echo "Attempting to get the TV name..."
-TV_NAME=$(curl -s http://$TV_IP:8001/api/v2/ | jq -r '.device.modelName')
+TV_NAME=$(sdb devices | grep -E '\s{5}\w+' -o | xargs)
 
 if [ -z "$TV_NAME" ]; then
     echo "We were unable to find the TV name.";
